0bcc281b42cfa742c8a638f4a3128a5e86f007dd
karunaesh <karunaesh@gmail.com>
2025-01-07 16:38:10 +0530

    made the chnages

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-2-instance-1.c7ui86ykg677.eu-central-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'KMV4V76vn4amfnTyPI5y',
+    DB_DATABASE : 'webappdb'
+});
